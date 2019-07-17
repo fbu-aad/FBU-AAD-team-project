bf7f6f63a1e4680790ab524e91b1c2fdba982e8f
Denize Patric Bautista Ignacio <denizeignacio@gmail.com>
2019-07-17 15:13:17 -0700

    Updated to androidx

--- app/src/main/java/com/example/una/adapters/VerticalRecyclerViewAdapter.java
+++ app/src/main/java/com/example/una/adapters/VerticalRecyclerViewAdapter.java
@@ -1,13 +1,14 @@
 package com.example.una.adapters;
 
 import android.content.Context;
-import android.support.annotation.NonNull;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.una.R;
 import com.example.una.RecyclerViewModels.HorizontalModel;
@@ -15,7 +16,7 @@ import com.example.una.RecyclerViewModels.VerticalModel;
 
 import java.util.ArrayList;
 
-import static android.support.v7.widget.LinearLayoutManager.HORIZONTAL;
+import static androidx.recyclerview.widget.RecyclerView.HORIZONTAL;
 
 public class VerticalRecyclerViewAdapter extends RecyclerView.Adapter<VerticalRecyclerViewAdapter.VerticalRVViewHolder> {
     Context context;
