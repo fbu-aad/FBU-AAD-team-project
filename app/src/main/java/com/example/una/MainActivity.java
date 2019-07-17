33f19d27570f4b681712146b08a1601de8c81684
alexandrashaw10 <alels4321@gmail.com>
2019-07-16 17:21:46 -0700

    import recommit of navigation bar

--- app/src/main/java/com/example/una/MainActivity.java
+++ app/src/main/java/com/example/una/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.una;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,41 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final FragmentManager fragmentManager = getSupportFragmentManager();
+
+        // TODO change 1st fragment to Denize's HomePageFragment
+        final Fragment homePageFragment = new QuickDonateFragment();
+        final Fragment quickDonateFragment = new QuickDonateFragment();
+        final Fragment mapsViewFragment = new MapsViewFragment();
+        final Fragment impactFragment = new ImpactFragment();
+
+        BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_navigation_home);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                Fragment fragment;
+                switch (menuItem.getItemId()) {
+                    case R.id.action_home_page:
+                        fragment = homePageFragment;
+                        break;
+                    case R.id.action_quick_donate:
+                        fragment = quickDonateFragment;
+                        break;
+                    case R.id.action_maps_view:
+                        fragment = mapsViewFragment;
+                        break;
+                    case R.id.action_impact:
+                        fragment = impactFragment;
+                        break;
+                    default:
+                            fragment = homePageFragment;
+                        break;
+                }
+                fragmentManager.beginTransaction().replace(R.id.flMainFragments, fragment).commit();
+                return true;
+            }
+        });
+        bottomNavigationView.setSelectedItemId(R.id.action_home_page);
     }
 }
