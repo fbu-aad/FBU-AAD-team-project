f8a838aa44743e5a4076991804f9d110c338e4c6
alexandrashaw10 <alels4321@gmail.com>
2019-07-17 18:36:27 -0700

    added Facebook and Google sign in abilities through Firebase

--- app/src/main/java/com/example/una/LoginActivity.java
+++ app/src/main/java/com/example/una/LoginActivity.java
@@ -1,51 +1,85 @@
 package com.example.una;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Toast;
 
-import com.facebook.CallbackManager;
-import com.facebook.FacebookCallback;
-import com.facebook.FacebookException;
-import com.facebook.login.LoginManager;
-import com.facebook.login.LoginResult;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.ErrorCodes;
+import com.firebase.ui.auth.IdpResponse;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class LoginActivity extends AppCompatActivity {
 
-    CallbackManager callbackManager;
+    private static final int RC_SIGN_IN = 123;
+
+    Button fbLoginButton;
+    Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        context = this;
 
-
-        callbackManager = CallbackManager.Factory.create();
-
-        LoginManager.getInstance().registerCallback(callbackManager,
-                new FacebookCallback<LoginResult>() {
-                    @Override
-                    public void onSuccess(LoginResult loginResult) {
-                        // App code
-                    }
-
-                    @Override
-                    public void onCancel() {
-                        // App code
-                    }
-
-                    @Override
-                    public void onError(FacebookException exception) {
-                        // App code
-                    }
-                });
+        fbLoginButton = findViewById(R.id.fbLoginButton);
+        startLogin();
     }
 
+    // start the login process
+    private void startLogin() {
+        // Choose authentication providers
+        List<AuthUI.IdpConfig> providers = Arrays.asList(
+                new AuthUI.IdpConfig.EmailBuilder().build(),
+                new AuthUI.IdpConfig.GoogleBuilder().build(),
+                new AuthUI.IdpConfig.FacebookBuilder().build());
+
+        // Create and launch sign-in intent
+        startActivityForResult(
+                AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setAvailableProviders(providers)
+                        .build(),
+                RC_SIGN_IN);
+    }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        callbackManager.onActivityResult(requestCode, resultCode, data);
         super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RC_SIGN_IN) {
+            IdpResponse response = IdpResponse.fromResultIntent(data);
+
+            if (resultCode == RESULT_OK) {
+                // user is signed in
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                startHomeActivity(user);
+            } else {
+                // Sign in failed
+                if (response == null) {
+                    Toast.makeText(this, "Sign in Cancelled", Toast.LENGTH_SHORT).show();
+                    return;
+                } else if (response.getError().getErrorCode() == ErrorCodes.NO_NETWORK) {
+                    Toast.makeText(this, "No Internet Connection", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(this, "Unable to Complete Sign In", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
     }
+
+    private void startHomeActivity(FirebaseUser user) {
+        Intent homeIntent = new Intent(this, MainActivity.class);
+        homeIntent.putExtra("user", user);
+        startActivity(homeIntent);
+    }
+
 }
