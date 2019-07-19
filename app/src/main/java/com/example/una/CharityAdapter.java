d0864076dc175b81b06eb570ee0cd12b0b71e466
alexandrashaw10 <alels4321@gmail.com>
2019-07-19 14:02:31 -0700

    working JSON response for Lists part 2

--- app/src/main/java/com/example/una/CharityAdapter.java
+++ app/src/main/java/com/example/una/CharityAdapter.java
@@ -1,4 +1,10 @@
 package com.example.una;
 
+import java.util.ArrayList;
+
 public class CharityAdapter {
+    ArrayList<Object> charities = new ArrayList<>();
+
+    // initialize with list
+    public CharityAdapter(ArrayList<Object> charities) { this.charities = charities; }
 }
