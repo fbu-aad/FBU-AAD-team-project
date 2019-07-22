bdb44bcd8b50a56b026c58b04fa4195c3b557d7d
Denize Patric Bautista Ignacio <denizeignacio@gmail.com>
2019-07-22 13:17:41 -0700

    Changed BugApplication name and removed extra Buglife dependency

--- app/src/main/java/com/example/una/UnaApplication.java
+++ app/src/main/java/com/example/una/UnaApplication.java
@@ -5,12 +5,12 @@ import android.app.Application;
 import com.buglife.sdk.Buglife;
 import com.buglife.sdk.InvocationMethod;
 
-public class BugApplication extends Application {
+public class UnaApplication extends Application {
     @Override
     public void onCreate() {
         // Extended Application to use Buglife
         super.onCreate();
-        Buglife.initWithEmail(this, getString(R.string.denize));
+        Buglife.initWithEmail(this, getString(R.string.bug_report_email));
         Buglife.setInvocationMethod(InvocationMethod.SHAKE);
     }
 }
