3214a131f5a5d5012dc53f40669780e4c10e8de3
ajin12 <amy_jin@college.harvard.edu>
2019-07-24 15:51:07 -0700

    modify challenges item layout and add placeholder image

--- app/src/main/java/com/example/una/Viewholders/ChallengeViewHolder.java
+++ app/src/main/java/com/example/una/Viewholders/ChallengeViewHolder.java
@@ -1,6 +1,7 @@
 package com.example.una.Viewholders;
 
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -15,17 +16,24 @@ import butterknife.ButterKnife;
 public class ChallengeViewHolder extends RecyclerView.ViewHolder {
     @BindView(R.id.tvChallengeTitle) TextView tvChallengeTitle;
     @BindView(R.id.tvChallengeDescription) TextView tvChallengeDescription;
+    @BindView(R.id.ivChallenge) ImageView ivChallenge;
+
     public ChallengeViewHolder(@NonNull View itemView) {
         super(itemView);
         ButterKnife.bind(this, itemView);
         itemView.setOnClickListener(new ChallengeClickListener());
     }
+
     public TextView getTvChallengeTitle() {
         return tvChallengeTitle;
     }
+
     public TextView getTvChallengeDescription() {
         return tvChallengeDescription;
     }
+
+    public ImageView getChallengeImage() { return ivChallenge; }
+
     static class ChallengeClickListener implements View.OnClickListener {
         @Override
         public void onClick(View v) {
--- app/src/main/java/com/example/una/adapters/StreaksComplexRecyclerViewAdapter.java
+++ app/src/main/java/com/example/una/adapters/StreaksComplexRecyclerViewAdapter.java
@@ -1,11 +1,13 @@
 package com.example.una.adapters;
 
+import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.bumptech.glide.Glide;
 import com.example.una.Challenge;
 import com.example.una.R;
 import com.example.una.Viewholders.ChallengeViewHolder;
@@ -17,6 +19,7 @@ public class StreaksComplexRecyclerViewAdapter extends RecyclerView.Adapter<Recy
     // The challenges to display in your RecyclerView
     private List<Object> items;
     private final int IMAGE = 0, CHALLENGE = 1;
+    Context context;
 
     public StreaksComplexRecyclerViewAdapter(List<Object> items) {
         this.items = items;
@@ -38,6 +41,7 @@ public class StreaksComplexRecyclerViewAdapter extends RecyclerView.Adapter<Recy
 
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup viewGroup, int viewType) {
+        context = viewGroup.getContext();
         RecyclerView.ViewHolder viewHolder;
         LayoutInflater inflater = LayoutInflater.from(viewGroup.getContext());
         if (viewType == CHALLENGE) {
@@ -63,14 +67,20 @@ public class StreaksComplexRecyclerViewAdapter extends RecyclerView.Adapter<Recy
     }
 
     private void configureChallengeViewHolder(ChallengeViewHolder vhChallenge, int position) {
-        Challenge user = (Challenge) items.get(position);
-        if (user != null) {
-            vhChallenge.getTvChallengeTitle().setText("Challenge: " + user.getChallengeTitle());
-            vhChallenge.getTvChallengeDescription().setText("Description: " + user.getChallengeDescription());
+        Challenge challenge = (Challenge) items.get(position);
+        if (challenge != null) {
+            vhChallenge.getTvChallengeTitle().setText("Challenge: " + challenge.getChallengeTitle());
+            vhChallenge.getTvChallengeDescription().setText("Description: " + challenge.getChallengeDescription());
+
+            // set challenge image placeholder
+            int random = (int) (Math.random() * 100 + 1);
+            Glide.with(context)
+                    .load("https://picsum.photos/id/" + random + "/400/200")
+                    .into(vhChallenge.getChallengeImage());
         }
     }
 
     private void configureStreakViewHolder(StreaksViewHolder vhStreak) {
-        vhStreak.getIvCharityImage().setImageResource(R.drawable.ic_cuppa_24dp);
+        vhStreak.getIvCharityImage().setImageResource(R.drawable.ic_streak_black_24dp);
     }
 }
