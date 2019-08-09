1f498fd38038fa10b42e64e5230a61b32b7efc12
ajin12 <amy_jin@college.harvard.edu>
2019-08-09 12:29:28 -0700

    polish donation history item ui

--- app/src/main/java/com/example/una/TimeFormatHelper.java
+++ app/src/main/java/com/example/una/TimeFormatHelper.java
@@ -5,7 +5,7 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class TimeFormatHelper {
-    private static final String dayMonthYearPattern = "MM/dd/yyyy";
+    private static final String dayMonthYearPattern = "EEE, MMM d, yyyy";
     private static final String dayMonthYearHourMinutePattern = "MM/dd/yyyy HH:mm:ss";
     private static final DateFormat dayMonthYearFormat = new SimpleDateFormat(dayMonthYearPattern);
     private static final DateFormat dayMonthYearHourMinuteFormat = new SimpleDateFormat(dayMonthYearHourMinutePattern);
